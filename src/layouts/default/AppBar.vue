fd55094cdd49e435c45b08dbaedd3590f2730934
Summer-lights <1067088037@qq.com>
2023-09-22 20:07:32 +0800

    update bar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,11 +1,18 @@
 <template>
-  <v-app-bar flat>
+  <v-app-bar elevation="2">
     <v-app-bar-title>
-
+      <v-icon>
+        mdi-microsoft-powerpoint
+      </v-icon>
+      RoboPPT
     </v-app-bar-title>
+
+    <template v-slot:append>
+      <b>遥遥领先</b>
+    </template>
   </v-app-bar>
 </template>
 
 <script lang="ts" setup>
-  //
+//
 </script>
